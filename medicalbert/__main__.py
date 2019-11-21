43c9320d9bd140df0bcc3a0720dd74f880f36795
Andrew Patterson <andrewrpatterson@btinternet.com>
2019-11-21 20:02:02 +0000

    WIP

--- medicalbert/__main__.py
+++ medicalbert/__main__.py
@@ -6,23 +6,28 @@ import config
 from classifier import Classifier
 from trainer import Trainer
 
+from cliparse import setup_parser
+
 if __name__ == "__main__":
+    parser = setup_parser()
+    args = parser.parse_args()
     logging.info("Number of GPUS: {}".format(torch.cuda.device_count()))
 
     # load the data
-    datareader = DataReader(config.training_data, config.tokenizer, config.max_sequence_length, config.hyperparams['batch_size'])
+    if args.train:
+        datareader = DataReader(config.training_data, config.tokenizer, config.max_sequence_length, config.hyperparams['batch_size'])
 
-    # Build a classifier object to use
-    classifier = Classifier(config.hyperparams)
+        # Build a classifier object to use
+        classifier = Classifier(config.hyperparams)
 
-    # Load from checkpoint if we're using one (won't do anything if were not)
-    classifier.load_from_checkpoint()
+        # Load from checkpoint if we're using one (won't do anything if were not)
+        classifier.load_from_checkpoint()
 
-    # Pass the classifier to the trainer
-    trainer = Trainer(classifier, datareader, config.hyperparams)
+        # Pass the classifier to the trainer
+        trainer = Trainer(classifier, datareader, config.hyperparams)
 
-    # Do the training
-    trainer.run()
+        # Do the training
+        trainer.run()
 
 
 
---
+++ medicalbert/cliparser.py
@@ -0,0 +1,24 @@
+import argparse
+
+
+def setup_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--train_from_checkpoint",
+                        default=None,
+                        type=str,
+                        help="Continue training from a saved model.")
+
+    parser.add_argument("--train",
+                        action='store_true',
+                        help="Whether to run training.")
+
+    parser.add_argument("--do_eval",
+                        action='store_true',
+                        help="Whether to run eval on the dev set.")
+    parser.add_argument("--do_test",
+                        action='store_true',
+                        help="Whether to run eval on the test set.")
+    parser.add_argument("--do_lower_case",
+                        action='store_true',
+                        help="Set this flag if you are using an uncased model.")
