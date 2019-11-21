09483706f1a04ea0c51428259cbb307bea8fde09
Andrew Patterson <andrewrpatterson@btinternet.com>
2019-11-21 07:47:09 +0000

    WIP

--- medicalbert/config.py
+++ medicalbert/config.py
@@ -25,5 +25,5 @@ hyperparams['learning_rate'] = 0.00001
 hyperparams['num_warmup_steps'] = 10
 hyperparams['epochs'] = 5
 
-checkpoint_location = "/home/strychl3/data"
+checkpoint_location = "/home/strychl3/DATA"
 run_name = "tail-only-random"
