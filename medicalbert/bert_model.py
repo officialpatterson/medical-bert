71d3a86ed2bf9832360700ff97f0614dbca9896d
Andrew Patterson <andrewrpatterson@btinternet.com>
2019-11-21 21:40:18 +0000

    WIP

--- medicalbert/bert_model.py
+++ medicalbert/bert_model.py
@@ -37,7 +37,7 @@ class BertForSequenceClassification(BertPreTrainedModel):
         self.bert = BertModel(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
-        self.head = nn.Sigmoid()
+        self.head = nn.Softmax()
         self.init_weights()
 
     def forward(self, input_ids=None, attention_mask=None, token_type_ids=None,
