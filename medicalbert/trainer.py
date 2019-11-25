c45346db958332c99a5dfd20596ac43fb14ac13f
Andrew Patterson <andrewrpatterson@btinternet.com>
2019-11-25 21:55:23 +0000

    old bert approach

--- medicalbert/trainer.py
+++ medicalbert/trainer.py
@@ -54,6 +54,8 @@ class Trainer:
                     batch = tuple(t.to(device) for t in batch)
                     labels, features = batch
 
+                    print(features.shape)
+
                     outputs = model(features, labels)
 
                     loss = outputs[0]
