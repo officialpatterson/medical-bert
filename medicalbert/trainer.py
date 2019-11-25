a30b3e7c28b30a383968950cf73f9682537cafc1
Andrew Patterson <andrewrpatterson@btinternet.com>
2019-11-25 21:50:12 +0000

    old bert approach

--- medicalbert/trainer.py
+++ medicalbert/trainer.py
@@ -3,9 +3,10 @@ import logging
 import os
 
 import torch
+from pytorch_transformers import BertForSequenceClassification
 from tqdm import tqdm, trange
 from statistics import mean
-
+from pytorch_pretrained_bert import BertAdam
 class Trainer:
     def __init__(self, config, classifier, datareader):
         self.classifier = classifier
@@ -13,11 +14,33 @@ class Trainer:
         self.config = config
 
     def run(self):
+
+        model = BertForSequenceClassification.from_pretrained(self.config['pretrained_model'])
+
+        # To reproduce BertAdam specific behavior set correct_bias=False
+        # self.optimizer = AdamW(self.model.parameters(), lr=self.config['learning_rate'], correct_bias=False)
+
+        # PyTorch scheduler
+        num_steps = int(self.config['num_train_examples'] / self.config['train_batch_size'] /
+                        self.config['gradient_accumulation_steps']) * \
+                    self.config['epochs']
+
+        optimizer_grouped_parameters = [
+            {'params': self.model.parameters(), 'lr': self.config['learning_rate']}
+        ]
+
+        optimizer = BertAdam(optimizer_grouped_parameters,
+                                  lr=self.config['learning_rate'],
+                                  warmup=self.config['warmup_proportion'],
+                                  t_total=num_steps)
+
+
         logging.info("Training...")
         # Put the classifier in training mode.
         device = torch.device(self.config['device'])
 
-        self.classifier.set_train_mode(device)
+        model.train()
+        model.to(device)
 
         batch_losses = []
         for _ in trange(self.classifier.epochs, int(self.config['epochs']), desc="Epoch"):
@@ -31,8 +54,7 @@ class Trainer:
                     batch = tuple(t.to(device) for t in batch)
                     labels, features = batch
 
-
-                    outputs = self.classifier.forward_pass(features, labels)
+                    outputs = model(features, labels)
 
                     loss = outputs[0]
 
@@ -47,7 +69,8 @@ class Trainer:
                         batch_losses.append(mean(batche))
                         epoch_loss.append(mean(batche))
                         # Update the model gradients
-                        self.classifier.update_gradients()
+                        optimizer.step()
+                        optimizer.zero_grad()
             print("EPOCH LOSS: {}\n".format(mean(epoch_loss)))
             epoch_loss = []
             with open(os.path.join(self.config['output_dir'], self.config['experiment_name'], "batch_loss.csv"), "a") as f:
