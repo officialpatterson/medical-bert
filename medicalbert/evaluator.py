d2f7fb9eff08c7630a84a87ced744cd879c1c520
Andrew Patterson <andrewrpatterson@btinternet.com>
2019-12-11 20:53:50 +0000

    Added second data reader.

--- medicalbert/evaluator.py
+++ medicalbert/evaluator.py
@@ -46,7 +46,9 @@ class Evaluator:
             input_ids, input_mask, segment_ids, label_ids = batch
 
             with torch.no_grad():
-                tmp_eval_loss, logits = self.model(input_ids, segment_ids, input_mask, label_ids)
+                out = self.model(input_ids, segment_ids, input_mask, label_ids)
+                tmp_eval_loss = out[0]
+                logit = out[1]
 
             all_losses.append(tmp_eval_loss.mean().item())
             logits = logits.detach().cpu().numpy()
