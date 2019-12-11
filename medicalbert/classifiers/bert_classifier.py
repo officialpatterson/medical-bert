f720de40e352214e8a252969cc3a32b942e66a68
Andrew Patterson <andrewrpatterson@btinternet.com>
2019-12-11 19:48:02 +0000

    Added second data reader.

--- medicalbert/classifiers/bert_classifier.py
+++ medicalbert/classifiers/bert_classifier.py
@@ -64,7 +64,7 @@ class BertGeneralClassifier:
                     print("input_mask_shape: {}".format(input_mask.shape))
                     print("segment_ids.shape: {}".format(segment_ids.shape))
                     print("label_ids.shape: {}".format(label_ids.shape))
-                    outputs = self.model(input_ids, segment_ids, input_mask, label_ids)
+                    outputs = self.model(input_ids, segment_ids, input_mask, labels=label_ids)
 
                     loss = outputs[0]
 
