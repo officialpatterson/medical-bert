335419442fcd20b9362fdae94ec244267a3ecf76
Andrew Patterson <andrewrpatterson@btinternet.com>
2019-12-11 19:10:07 +0000

    Added second data reader.

--- medicalbert/datareader/data_reader_factory.py
+++ medicalbert/datareader/data_reader_factory.py
@@ -4,7 +4,7 @@ from datareader.datareader import DataReader
 
 class DataReaderFactory:
     def __init__(self, config):
-        self._datareaders = {"one-doc": DataReader, "pair": PairReader}
+        self._datareaders = {"one-doc": DataReader}
         self.config = config
 
     def register_datareader(self, name, datareader):
