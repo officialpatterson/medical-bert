796faeb61997f4710a68d8d84c70fb2a29c9d634
Andrew Patterson <andrewrpatterson@btinternet.com>
2020-01-12 12:27:29 +0000

    Commented out the layer deletion step for comparison.

--- run-general.sh
+++ run-general.sh
@@ -1,3 +1,7 @@
-python medicalbert/ --experiment_name bert-uncase-lr5e5B64 --train --eval --target readm_30d --classifier bert-general --learning_rate 0.00005 --train_batch_size 64 --gradient_accumulation_steps 64;
-python medicalbert/ --experiment_name bert-uncase-lr5e5B32 --train --eval --target readm_30d --classifier bert-general --learning_rate 0.00005 --train_batch_size 32 --gradient_accumulation_steps 32;
-python medicalbert/ --experiment_name bert-uncase-lr2e5B32 --train --eval --target readm_30d --classifier bert-general --learning_rate 0.00002 --epochs 10;
+python medicalbert/ --experiment_name bert-uncase-lr5e5B16-12-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 12;
+python medicalbert/ --experiment_name bert-uncase-lr5e5B16-10-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 10;
+python medicalbert/ --experiment_name bert-uncase-lr5e5B16-8-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 8;
+python medicalbert/ --experiment_name bert-uncase-lr5e5B16-6-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 6;
+python medicalbert/ --experiment_name bert-uncase-lr5e5B16-4-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 4;
+python medicalbert/ --experiment_name bert-uncase-lr5e5B16-2-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 2;
+python medicalbert/ --experiment_name bert-uncase-lr5e5B16-1-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 1;
