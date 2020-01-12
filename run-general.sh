95d32d815c4d7fac85e3ec02186157027fbdf03b
Andrew Patterson <andrewrpatterson@btinternet.com>
2020-01-12 12:28:39 +0000

    runner bash script has been updated to reflect new experiments

--- run-general.sh
+++ run-general.sh
@@ -1,7 +1,7 @@
-python medicalbert/ --experiment_name bert-uncase-lr5e5B16-12-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 12;
-python medicalbert/ --experiment_name bert-uncase-lr5e5B16-10-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 10;
-python medicalbert/ --experiment_name bert-uncase-lr5e5B16-8-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 8;
-python medicalbert/ --experiment_name bert-uncase-lr5e5B16-6-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 6;
-python medicalbert/ --experiment_name bert-uncase-lr5e5B16-4-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 4;
-python medicalbert/ --experiment_name bert-uncase-lr5e5B16-2-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 2;
-python medicalbert/ --experiment_name bert-uncase-lr5e5B16-1-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 1;
+python medicalbert/ --experiment_name bert-uncase-lr5e5B16-12-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 12 --epochs 3;
+python medicalbert/ --experiment_name bert-uncase-lr5e5B16-10-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 10 --epochs 3;
+python medicalbert/ --experiment_name bert-uncase-lr5e5B16-8-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 8 --epochs 3;
+python medicalbert/ --experiment_name bert-uncase-lr5e5B16-6-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 6 --epochs 3;
+python medicalbert/ --experiment_name bert-uncase-lr5e5B16-4-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 4 --epochs 3;
+python medicalbert/ --experiment_name bert-uncase-lr5e5B16-2-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 2 --epochs 3;
+python medicalbert/ --experiment_name bert-uncase-lr5e5B16-1-layers --train --eval --target readm_30d --classifier bert-general --learning_rate 0.000005 --num_layers 1 --epochs 3;
