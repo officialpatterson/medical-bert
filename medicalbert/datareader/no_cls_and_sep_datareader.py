3c6c1b00f027a09ae0082ebdcc14e7ec57394232
Andrew Patterson <andrewrpatterson@btinternet.com>
2020-01-13 23:15:14 +0000

    debugging: no sep and cls datareader.

--- medicalbert/datareader/no_cls_and_sep_datareader.py
+++ medicalbert/datareader/no_cls_and_sep_datareader.py
@@ -66,11 +66,9 @@ def convert_example_to_feature(example, label, max_seq_length, tokenizer):
     tokens = []
     segment_ids = []
 
-    segment_ids.append(0)
     for token in tokens_a:
         tokens.append(token)
         segment_ids.append(0)
-    segment_ids.append(0)
 
     if tokens_b:
         #TODO: likewise
@@ -86,13 +84,15 @@ def convert_example_to_feature(example, label, max_seq_length, tokenizer):
     # tokens are attended to.
     input_mask = [1] * len(input_ids)
 
+    assert len(input_ids) == len(input_mask)
+    assert len(input_mask) == len(segment_ids)
+
     # Zero-pad up to the sequence length.
     while len(input_ids) < max_seq_length:
         input_ids.append(0)
         input_mask.append(0)
         segment_ids.append(0)
 
-    print(len(input_ids))
     assert len(input_ids) == max_seq_length
     assert len(input_mask) == max_seq_length
     assert len(segment_ids) == max_seq_length
