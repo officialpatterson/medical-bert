05a44cf4014923b226a5e6165ec939cbccdcb269
Andrew Patterson <andrewrpatterson@btinternet.com>
2020-01-26 16:25:12 +0000

    using bertadam

--- medicalbert/classifiers/classifier.py
+++ medicalbert/classifiers/classifier.py
@@ -24,7 +24,7 @@ class Classifier:
                     batch = tuple(t.to(device) for t in batch)
                     input_ids, input_mask, segment_ids, label_ids = batch
 
-                    loss = self.model(input_ids=input_ids, token_type_ids=segment_ids, attention_mask=input_mask, labels=label_ids)[0]
+                    loss =  self.model(input_ids, labels=label_ids)[0]
 
                     # Statistics
                     batche.append(loss.item())
