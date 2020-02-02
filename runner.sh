516dccec7e2c910abecf93cec5c0815568744c05
Andrew Patterson <andrewrpatterson@btinternet.com>
2020-02-02 19:21:03 +0000

    Hopefully this is it.

--- runner.sh
+++ runner.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-python medicalbert/ --experiment_name alsentzer1e4 --learning_rate 0.001 --tokenizer /home/apatterson/pretrained_bert_tf/biobert_pretrain_output_disch_100000 --pretrained_model /home/apatterson/pretrained_bert_tf/biobert_pretrain_output_disch_100000 --train --eval --classifier bert-general;
 python medicalbert/ --experiment_name alsentzer1e4 --learning_rate 0.0001 --tokenizer /home/apatterson/pretrained_bert_tf/biobert_pretrain_output_disch_100000 --pretrained_model /home/apatterson/pretrained_bert_tf/biobert_pretrain_output_disch_100000 --train --eval --classifier bert-general;
-python medicalbert/ --experiment_name alsentzer1e4 --learning_rate 0.00001 --tokenizer /home/apatterson/pretrained_bert_tf/biobert_pretrain_output_disch_100000 --pretrained_model /home/apatterson/pretrained_bert_tf/biobert_pretrain_output_disch_100000 --train --eval --classifier bert-general;
+python medicalbert/ --experiment_name general1e4 --learning_rate 0.0001 --train --eval --classifier bert-general;
+python medicalbert/ --experiment_name random1e4 --learning_rate 0.0001  --train --eval --classifier bert-random;
 
-python medicalbert/ --experiment_name alsentzer-mean-pool-tokens --learning_rate 0.001 --tokenizer /home/apatterson/pretrained_bert_tf/biobert_pretrain_output_disch_100000 --pretrained_model /home/apatterson/pretrained_bert_tf/biobert_pretrain_output_disch_100000 --train --eval --classifier bert-mean-pool;
 python medicalbert/ --experiment_name alsentzer-mean-pool-tokens --learning_rate 0.0001 --tokenizer /home/apatterson/pretrained_bert_tf/biobert_pretrain_output_disch_100000 --pretrained_model /home/apatterson/pretrained_bert_tf/biobert_pretrain_output_disch_100000 --train --eval --classifier bert-mean-pool;
-python medicalbert/ --experiment_name alsentzer-mean-pool-tokens --learning_rate 0.00001 --tokenizer /home/apatterson/pretrained_bert_tf/biobert_pretrain_output_disch_100000 --pretrained_model /home/apatterson/pretrained_bert_tf/biobert_pretrain_output_disch_100000 --train --eval --classifier bert-mean-pool;
