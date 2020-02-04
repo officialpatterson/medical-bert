7e3fb944c3de24490741a0fdf72cfca8e92adceb
Andrew Patterson <andrewrpatterson@btinternet.com>
2020-02-04 19:44:06 +0000

    Putting in BertAdAam

--- medicalbert/classifiers/bert_classifier.py
+++ medicalbert/classifiers/bert_classifier.py
@@ -2,6 +2,7 @@ import torch
 from classifiers.bert_model import BertForSequenceClassification
 from classifiers.classifier import Classifier
 from classifiers.util import deleteEncodingLayers
+from pytorch_pretrained_bert import BertAdam
 
 
 class BertGeneralClassifier(Classifier):
@@ -11,7 +12,18 @@ class BertGeneralClassifier(Classifier):
 
         # here, we can do some layer removal if we want to
         # setup the optimizer
-        self.optimizer = torch.optim.Adam(self.model.parameters(), self.config['learning_rate'])
+        num_steps = int(self.config['num_train_examples'] / self.config['train_batch_size'] /
+                        self.config['gradient_accumulation_steps']) * \
+                    self.config['epochs']
+
+        optimizer_grouped_parameters = [
+            {'params': self.model.parameters(), 'lr': self.config['learning_rate']}
+        ]
+
+        self.optimizer = BertAdam(optimizer_grouped_parameters,
+                                  lr=self.config['learning_rate'],
+                                  warmup=self.config['warmup_proportion'],
+                                  t_total=num_steps)
 
         self.epochs = 0
         print(self.model)
--- medicalbert/classifiers/bert_random_classifier.py
+++ medicalbert/classifiers/bert_random_classifier.py
@@ -15,8 +15,18 @@ class BertRandomClassifier(Classifier):
 
         # here, we can do some layer removal if we want to
 
-        #setup the optimizer
-        self.optimizer = torch.optim.Adam(self.model.parameters(), self.config['learning_rate'])
+        num_steps = int(self.config['num_train_examples'] / self.config['train_batch_size'] /
+                        self.config['gradient_accumulation_steps']) * \
+                    self.config['epochs']
+
+        optimizer_grouped_parameters = [
+            {'params': self.model.parameters(), 'lr': self.config['learning_rate']}
+        ]
+
+        self.optimizer = BertAdam(optimizer_grouped_parameters,
+                                  lr=self.config['learning_rate'],
+                                  warmup=self.config['warmup_proportion'],
+                                  t_total=num_steps)
 
         self.epochs = 0
 
