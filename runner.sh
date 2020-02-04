11d11e95526d7c8dc62045c355256ddba11d4290
Andrew Patterson <andrewrpatterson@btinternet.com>
2020-02-04 19:55:39 +0000

    Putting in BertAdAam

--- runner.sh
+++ runner.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 python medicalbert/ --experiment_name alsentzer1e5 --learning_rate 0.00001 --tokenizer /home/apatterson/pretrained_bert_tf/biobert_pretrain_output_disch_100000 --pretrained_model /home/apatterson/pretrained_bert_tf/biobert_pretrain_output_disch_100000 --train --eval --classifier bert-general;
-python medicalbert/ --experiment_name general1e5 --learning_rate 0.00001 --train --eval --classifier bert-general;
-python medicalbert/ --experiment_name random1e5 --learning_rate 0.00001  --train --eval --classifier bert-random;
-
-python medicalbert/ --experiment_name alsentzer-mean-pool-tokens --learning_rate 0.00001 --tokenizer /home/apatterson/pretrained_bert_tf/biobert_pretrain_output_disch_100000 --pretrained_model /home/apatterson/pretrained_bert_tf/biobert_pretrain_output_disch_100000 --train --eval --classifier bert-mean-pool;
