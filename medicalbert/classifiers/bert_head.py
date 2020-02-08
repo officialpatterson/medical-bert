42dc7b3688ebebc6939932741bbe5249088ddf86
Andrew Patterson <andrewrpatterson@btinternet.com>
2020-02-08 09:39:57 +0000

    Putting in BertAdAam

--- medicalbert/classifiers/bert_head.py
+++ medicalbert/classifiers/bert_head.py
@@ -23,4 +23,4 @@ class BERTLSTMHead(nn.Module):
         print(hidden_states.shape)
         # We "pool" the model by simply taking all the hidden states and averaging them.
         pooled_output = self.dense(hidden_states)
-        return pooled_output[0]
+        return pooled_output
