7a9f56202ccebabddae320de9d14331131085355
Andrew Patterson <andrewrpatterson@btinternet.com>
2020-02-23 13:11:04 +0000

    Lots of refactoring of the classifiers package. We have also included the original implementation of sequence bert, albeit a variant.

--- medicalbert/classifiers/bert_sequence_classifier.py
+++ medicalbert/classifiers/bert_sequence_classifier.py
@@ -1,13 +1,13 @@
 import torch
 from classifiers.classifier import Classifier
 from classifiers.bert_sequence_wrapper import BertSequenceWrapper
-from classifiers.bert_mean_pool_classifier import BertModelMeanPooling
+from transformers import BertModel
 
 
 class BertSequenceClassifier(Classifier):
     def __init__(self, config):
         self.config = config
-        baseModel = BertModelMeanPooling.from_pretrained(self.config['pretrained_model'])
+        baseModel = BertModel.from_pretrained(self.config['pretrained_model'])
 
         self.model = BertSequenceWrapper(baseModel)
 
