e1cf61c9784c557265e7d68eab5a4027f042c81f
Andrew Patterson <andrewrpatterson@btinternet.com>
2020-02-23 14:31:32 +0000

    refactor data reader for easier testing

--- medicalbert/datareader/data_reader_factory.py
+++ medicalbert/datareader/data_reader_factory.py
@@ -1,11 +1,12 @@
 # Currently provides two approaches for data loading.
-from datareader.datareader import DataReader
-from datareader.no_cls_and_sep_datareader import NoClsAndSepDataReader
 from datareader.chunked_data_reader import ChunkedDataReader
+from datareader.no_cls_and_sep_datareader import NoClsAndSepDataReader
+from datareader.StandardDataReader import StandardDataReader
+
 
 class DataReaderFactory:
     def __init__(self, config):
-        self._datareaders = {"one-doc": DataReader, "no-cls-sep": NoClsAndSepDataReader, "chunked": ChunkedDataReader}
+        self._datareaders = {"one-doc": StandardDataReader, "no-cls-sep": NoClsAndSepDataReader, "chunked": ChunkedDataReader}
         self.config = config
 
     def register_datareader(self, name, datareader):
