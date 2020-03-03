35e0ca5448ebdc056e75c83380b40daef43ee6ad
Andrew Patterson <andrewrpatterson@btinternet.com>
2020-03-03 20:08:52 +0000

    Re-made the chunked data reader so that its similar to what we have for the standard data readers. Introudced a new classifier model.

--- medicalbert/classifiers/sequential/bert_sequence_wrapper.py
+++ medicalbert/classifiers/sequential/bert_sequence_wrapper.py
@@ -19,7 +19,7 @@ class BertSequenceWrapper(nn.Module):
             param.requires_grad = False
 
         # classification head.
-        self.fc = nn.Linear(768, 768)
+        self.fc = nn.Linear(num_sections*768, 768)
         self.fc1 = nn.Linear(768, 2)
 
     # The embedding layer will attempt to create a 'section vector'
@@ -34,12 +34,12 @@ class BertSequenceWrapper(nn.Module):
                 # bert_out should have shape (1,768) if only returning the CLS token.
                 bert_out = self.bert(section.unsqueeze(0))[2][12]
 
-                # bert out should be a 768-tensor
-                bert_outputs.append(bert_out)
+                # we take the average 768 word embedding as being representatitive of the section
+                bert_outputs.append(bert_out.mean(1))
 
             # Concatenate along tokens dimension and add to the list of examples
-            bert_layer.append(torch.cat(bert_outputs, 0).mean(1))
-        return torch.stack(bert_layer)
+            bert_layer.append(torch.cat(bert_outputs, dim=1))
+        return torch.cat(bert_layer)
 
     def forward(self, batch, labels):
 
